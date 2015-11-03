0fb8f4f2e5f24e540faed87ff100f6d57bc2f8e7
safareli <i.safareli@gmail.com>
2015-11-03 15:00:12 +0400

    add psd-extractor and cli

---
+++ bin/psd-extractor-cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var psdExtractor = require("../lib/psd-extractor.js")
+var program = require('commander');
+var R = require('ramda');
+var package = require('../package');
+
+
+function logAsJSON(obj){
+  console.log(JSON.stringify(obj,null,4));
+  return obj;
+}
+
+
+
+program
+  .version(package.version)
+  .description(package.description)
+  .usage('<file ...>')
+  .parse(process.argv);
+
+logAsJSON(
+  psdExtractor(
+    R.empty(program.args) 
+      ? '*.psd' 
+      : program.args
+  )
+)
